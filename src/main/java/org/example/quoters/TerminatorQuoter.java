c344b98df6831d6f7a64a7ceafa4f5c84cdeab90
varrivoda <varrivoda@yandex.ru>
2024-01-06 23:21:18 +1000

    3. BeanPostProcessor - init-метод

--- src/main/java/org/example/quoters/TerminatorQuoter.java
+++ src/main/java/org/example/quoters/TerminatorQuoter.java
@@ -1,8 +1,20 @@
 package org.example.quoters;
+import javax.annotation.PostConstruct;
 
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
+
     private String message;
 
     public void setMessage(String message) {
