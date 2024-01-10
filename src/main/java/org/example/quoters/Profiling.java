a16095eaf6baec53584d24a13ec4c9450dba6d67
varrivoda <varrivoda@yandex.ru>
2024-01-10 23:19:59 +0900

    4. BeanPostProcessor, afterInitialization - Proxy

---
+++ src/main/java/org/example/quoters/Profiling.java
@@ -0,0 +1,9 @@
+package org.example.quoters;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Profiling {
+}
